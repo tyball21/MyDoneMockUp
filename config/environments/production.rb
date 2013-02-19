7783a9ff9af629ef83dd36fd33e964d196dc0443
Ty Diamse <tylerdiamse@gmail.com>
2013-02-19 15:15:20 -0600

    10 try changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Done::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true #ask jeff about this
 
   # Generate digests for assets URLs
   config.assets.digest = true
